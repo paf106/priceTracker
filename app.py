be64942d3790e37304a5310b56532d42f07283a0
paf_106 <pabloavila106@gmail.com>
2020-09-04 21:59:24 +0200

    some changes made, working with MediaMarkt page

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://www.amazon.de/Syma-helicopter/dp/B00PJWNRDQ/ref=sr_1_5?__mk_de_DE=' \
-      '%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=syma+x5c&qid=1598887623&sr=8-5'
+URL = 'https://www.mediamarkt.es/es/product/_cargador-inal%C3%A1mbrico-belkin-boost-up-para-smartphones-qi-10w-pantalla-led-blanco-1457651.html'
 
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) App"
                          "leWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"}
 
 # request all the data from the website
-page = requests.get(URL, headers=headers)
+page = requests.get(URL)
 
-soup = BeautifulSoup(page.content, 'html.parser')
+soup = BeautifulSoup(page.text, 'html.parser')
 
-title = soup.find(id="priceblock_ourprice").get_text()
+title = soup.find('div', { "class" : "price"}).get_text()
 #price = soup.find(id="priceblock_ourprice")
 
 print(title)
