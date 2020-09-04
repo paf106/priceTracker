0b83afb8ef157a0e0b82d27d230e14c335d18863
paf_106 <pabloavila106@gmail.com>
2020-09-05 00:05:38 +0200

    header variable removed

--- app.py
+++ app.py
@@ -3,9 +3,6 @@ from bs4 import BeautifulSoup
 
 URL = 'https://www.mediamarkt.es/es/product/_cargador-inal%C3%A1mbrico-belkin-boost-up-para-smartphones-qi-10w-pantalla-led-blanco-1457651.html'
 
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) App"
-                         "leWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"}
-
 # request all the data from the website
 page = requests.get(URL)
 
