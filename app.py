eeb32f3cf27477807072eb88d7ce80cbd6e88a83
paf_106 <pabloavila106@gmail.com>
2020-09-05 20:23:56 +0200

    working with 2 websites

--- app.py
+++ app.py
@@ -1,18 +1,37 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://www.mediamarkt.es/es/product/_cargador-inal%C3%A1mbrico-belkin-boost-up-para-smartphones-qi-10w-pantalla-led-blanco-1457651.html'
+#URL = 'https://www.mediamarkt.es/es/product/_cargador-inal%C3%A1mbrico-belkin-boost-up-para-smartphones-qi-10w-pantalla-led-blanco-1457651.html'
+URL = str(input("Escribe la url: "))
 
-# request all the data from the website
+# Request all the data from the website
 page = requests.get(URL)
-
 soup = BeautifulSoup(page.content, 'html.parser')
 
-# We get the product title and the price
-productTitle = soup.find("h1").get_text()
-productPrice = soup.find('div', { "class" : "price"}).get_text()
+if (URL.find("mediamarkt") != -1):
+    # WORKING
+    # We get the product title and the price
+    productTitle = soup.find("h1").get_text()
+    productPrice = soup.find('div', { "class" : "price"}).get_text()
 
-print(productTitle.strip())
-print(productPrice.strip())
+    print("Nombre producto: "+productTitle.strip())
+    print("Precio: "+productPrice.strip())
+    
+elif (URL.find("aliexpress") != -1):
+    # NOT WORKING
+    # We get the product title and the price
+    #productPrice = soup.find('h1', { "class" : "product"}).get_text()
+    #productTitle = soup.find('h1').get_text()
+    productPrice = soup.find('div', { "class" : "product-info"}).get_text()
+    
+    print("Nombre producto: "+productTitle.strip())
+    #print("Precio: "+productPrice.strip())
 
+elif (URL.find("worten") != -1):
+    # WORKING
+    # We get the product title and the price
+    productTitle = soup.find("h1").get_text()
+    productPrice = soup.find('span', { "class" : "w-product__price__current"}).get_text()
 
+    print("Nombre producto: "+productTitle.strip())
+    print("Precio: "+productPrice.strip())
