2a9079ce84af29b7017a99dc55860be484a5877a
paf_106 <pabloavila106@gmail.com>
2020-09-07 14:17:57 +0200

    basic gui creation working

--- app.py
+++ app.py
@@ -1,63 +1,79 @@
 import requests
 from bs4 import BeautifulSoup
+from tkinter import *
 
-URL = str(input("Type an URL: "))
+# Main Window
+root = Tk()
+root.title("Price Tracker")
+root.geometry("400x400")
 
-# Request all the data from the website
-try:
-    page = requests.get(URL)
-    soup = BeautifulSoup(page.content, 'html.parser')
+# Search field
+inputField = Entry(root,width=38)
+inputField.insert(0,"Type an URL")
+inputField.grid(row=0, column=0)
 
-except requests.exceptions.MissingSchema:
-    print("Url not valid")
-except Exception:
-    print("Something went wrong")
+# Search button
+button_Search = Button(root, text="Search", command=lambda: checkPrice(inputField.get()))
+button_Search.grid(row=0, column=1)
 
-if (URL.find("mediamarkt") != -1):
-    # WORKING
-    # We get the product title and the price
+def checkPrice(URL):
+    # Request all the data from the website
     try:
-        productTitle = soup.find("h1").get_text()
-        productPrice = soup.find('div', { "class" : "price"}).get_text()
+        page = requests.get(URL)
+        soup = BeautifulSoup(page.content, 'html.parser')
 
-        print("Product name: "+productTitle.strip())
-        print("Price: "+productPrice.strip())
+    except requests.exceptions.MissingSchema:
+        print("Url not valid")
+    except Exception:
+        print("Something went wrong")
 
-    except AttributeError:
-        print("Product sold out or not trackable :(") 
-    
-elif (URL.find("aliexpress") != -1):
-    # NOT WORKING
-    # We get the product title and the price
-    #productPrice = soup.find('h1', { "class" : "product"}).get_text()
-    #productTitle = soup.find('h1').get_text()
-    productPrice = soup.find('div', { "class" : "product-info"}).get_text()
-    
-    print("Nombre producto: "+productTitle.strip())
-    #print("Precio: "+productPrice.strip())
+    if (URL.find("mediamarkt") != -1):
+        # WORKING
+        # We get the product title and the price
+        try:
+            productTitle = soup.find("h1").get_text()
+            productPrice = soup.find('div', { "class" : "price"}).get_text()
 
-elif (URL.find("worten") != -1):
-    # WORKING
-    # We get the product title and the price
-    try:
-        productTitle = soup.find("h1").get_text()
-        productPrice = soup.find('span', { "class" : "w-product__price__current"}).get_text()
+            print("Product name: "+productTitle.strip())
+            print("Price: "+productPrice.strip())
 
-        print("Product name: "+productTitle.strip())
-        print("Price: "+productPrice.strip())
-    
-    except AttributeError:
-        print("Product sold out or not trackable :(")
+        except AttributeError:
+            print("Product sold out or not trackable :(") 
+        
+    elif (URL.find("aliexpress") != -1):
+        # NOT WORKING
+        # We get the product title and the price
+        #productPrice = soup.find('h1', { "class" : "product"}).get_text()
+        #productTitle = soup.find('h1').get_text()
+        productPrice = soup.find('div', { "class" : "product-info"}).get_text()
+        
+        print("Nombre producto: "+productTitle.strip())
+        #print("Precio: "+productPrice.strip())
 
-elif (URL.find("pccomponentes") != -1):
-    # WORKING
-    # We get the product title and the price
-    try:
-        productTitle = soup.find("h1").get_text()
-        productPrice = soup.find('div', {"id" : "precio-main"}).get_text()
+    elif (URL.find("worten") != -1):
+        # WORKING
+        # We get the product title and the price
+        try:
+            productTitle = soup.find("h1").get_text()
+            productPrice = soup.find('span', { "class" : "w-product__price__current"}).get_text()
 
-        print("Product name: "+productTitle.strip())
-        print("Price: "+productPrice.strip())
-    
-    except AttributeError:
-        print("Product sold out or not trackable :(")
+            print("Product name: "+productTitle.strip())
+            print("Price: "+productPrice.strip())
+        
+        except AttributeError:
+            print("Product sold out or not trackable :(")
+
+    elif (URL.find("pccomponentes") != -1):
+        # WORKING
+        # We get the product title and the price
+        try:
+            productTitle = soup.find("h1").get_text()
+            productPrice = soup.find('div', {"id" : "precio-main"}).get_text()
+
+            print("Product name: "+productTitle.strip())
+            print("Price: "+productPrice.strip())
+        
+        except AttributeError:
+            print("Product sold out or not trackable :(")
+
+root.mainloop()
