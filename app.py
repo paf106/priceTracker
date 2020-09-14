d98b9509cd902a23d5a9f8fb438b10556cd2931d
paf_106 <pabloavila106@gmail.com>
2020-09-15 01:32:02 +0200

    few bugs fixed, still remain a big one

--- app.py
+++ app.py
@@ -2,11 +2,18 @@ import requests
 from bs4 import BeautifulSoup
 from tkinter import *
 from tkinter import messagebox
+import os
 
 # Main Window
 root = Tk()
 root.title("Price Tracker")
-root.geometry("502x450")
+root.geometry("620x450")
+
+# Root frame
+dataFrame = LabelFrame(root)
+label_dataFrame = Label(dataFrame,text="Only works with PcComponentes, Worten and Mediamarkt")
+label_dataFrame.grid()
+dataFrame.grid(row=2, column=0, columnspan=3, sticky="WE")
 
 # History of products to be tracked
 history = []
@@ -14,10 +21,21 @@ history = []
 # Favourite products
 favourite = []
 
+# Check if "favouriteProducts.txt" exists and append the items to a list
+if os.path.isfile("favouriteProducts.txt"):
+    with open("favouriteProducts.txt", "r") as f:
+        tempProducts = f.read()
+        tempProducts = tempProducts.split(",")
+        favourite = [x for x in tempProducts if x.strip()]
+        f.close()
+
 # Search field
 inputField = Entry(root,width=38)
-inputField.insert(0,"Type an URL")
-inputField.grid(row=0, column=0, ipady=4, pady=5, padx=2, sticky="W")
+inputField.grid(row=0, column=0, ipady=4, pady=5, padx=2, sticky="WE")
+
+def updateScreen(frame):
+    for widget in frame.winfo_children():
+        widget.destroy()
 
 def showHistory():
     historyWindow = Toplevel(root)
@@ -28,16 +46,22 @@ def showHistory():
         label_url = Label(historyWindow,text=link)
         label_url.pack()
         
+def deleteFavouriteProduct(product):
+    pass
+    
 
 def addFavouriteProduct():
     urlToAdd = inputField_favouriteProduct.get()
     if (len(urlToAdd)!= 0 and urlToAdd.find(" ") == -1):
-        button_searchFavourite = Button(favouriteWindow,text=inputField_favouriteProduct.get(),
-        command= lambda:checkPrice(urlToAdd))
+        button_searchFavourite = Button(favouriteWindow, text=urlToAdd, command= lambda:checkPrice(urlToAdd))
         button_searchFavourite.grid()
+        
+                # Add favourite products to a list
+        favourite.append(urlToAdd)
+        print(favourite)
     else:
         messagebox.showinfo("Empty field","You must write something")
-        
+
 def showFavourite():
     global favouriteWindow
     favouriteWindow = Toplevel(root)
@@ -47,17 +71,22 @@ def showFavourite():
     # Field to write an url
     global inputField_favouriteProduct
     inputField_favouriteProduct = Entry(favouriteWindow,width=38)
-    inputField_favouriteProduct.insert(0,"Type an URL")
     inputField_favouriteProduct.grid(row=0, column=0, pady=5, padx=3, ipady=4)
 
     # Add button
     button_Add = Button(favouriteWindow, text="+", command=addFavouriteProduct)
-    button_Add.grid(row=0, column=2)
+    button_Add.grid(row=0, column=2, sticky="WE")
 
     # Delete button
     # Delete the content of the search field
     button_deleteFavourite = Button(favouriteWindow, text="X", command=lambda: button_clear(inputField_favouriteProduct))
     button_deleteFavourite.grid(row=0, column=1, sticky="WE")
+
+    for x in favourite:
+        button_favouriteProduct = Button(favouriteWindow,text=x, command = lambda: checkPrice(x)).grid()
+
+
+    
 
 def clearHistory():
     history.clear()
@@ -67,9 +96,13 @@ def button_clear(entryToClear):
     entryToClear.delete(0, END)
 
 def printProductData(name, price):
+    global label_productName
+    global label_productPrice
 
-    label_productName = Label(root, text="Product name: "+ name[:45]+"...")
-    label_productPrice = Label(root, text="Price: "+ price)
+    updateScreen(dataFrame)
+
+    label_productName = Label(dataFrame, text="Product name: "+ name[:57]+"...")
+    label_productPrice = Label(dataFrame, text="Price: "+ price)
 
     label_productName.grid(row=2, column=0)
     label_productPrice.grid(row=3, column=0)
@@ -86,11 +119,11 @@ button_favourite.grid(row=1, column=0, sticky="EW", padx=2)
 
 # History button
 button_history = Button(root, text="History", command=showHistory)
-button_history.grid(row=1, column=1)
+button_history.grid(row=1, column=1, sticky="EW")
 
 # Clear history button
 button_clearHistory = Button(root, text="Clear history", command=clearHistory)
-button_clearHistory.grid(row=1, column=2)
+button_clearHistory.grid(row=1, column=2, sticky="EW")
 
 # Search button
 button_Search = Button(root, text="Search", command=lambda: checkPrice(inputField.get()))
@@ -101,7 +134,57 @@ def checkPrice(URL):
     try:
         page = requests.get(URL)
         soup = BeautifulSoup(page.content, 'html.parser')
-    
+
+        if (URL.find("mediamarkt") != -1):
+            # WORKING
+            # We get the product title and the price
+            try:
+                productTitle = soup.find("h1").get_text()
+                productPrice = soup.find('div', { "class" : "price"}).get_text()
+
+                printProductData(productTitle.strip(), productPrice.strip())
+
+                history.append(URL)
+
+            except AttributeError:
+                messagebox.showinfo("Info", "Product sold out or not trackable :(")
+            
+        elif (URL.find("aliexpress") != -1):
+            # NOT WORKING
+            # We get the product title and the price
+            #productPrice = soup.find('h1', { "class" : "product"}).get_text()
+            productTitle = soup.find('span', { "class" : "price"}).get_text()
+            
+            print("Nombre producto: "+productTitle.strip())
+            #print("Precio: "+productPrice.strip())
+        
+        elif (URL.find("worten") != -1):
+            # WORKING
+            # We get the product title and the price
+            try:
+                productTitle = soup.find("h1").get_text()
+                productPrice = soup.find('span', { "class" : "w-product__price__current"}).get_text()
+
+                printProductData(productTitle.strip(), productPrice.strip())
+
+                history.append(URL)
+            
+            except AttributeError:
+                messagebox.showinfo("Info", "Product sold out or not trackable :(")
+
+        elif (URL.find("pccomponentes") != -1):
+            # WORKING
+            # We get the product title and the price
+            try:
+                productTitle = soup.find("h1").get_text()
+                productPrice = soup.find('div', {"id" : "precio-main"}).get_text()
+
+                printProductData(productTitle.strip(), productPrice.strip())
+
+                history.append(URL)
+            
+            except AttributeError:
+                messagebox.showinfo("Info", "Product sold out or not trackable :(")
     # Errors handling
     except requests.exceptions.MissingSchema:
         messagebox.showwarning("Warning", "URL not valid")
@@ -112,55 +195,10 @@ def checkPrice(URL):
     except Exception:
         messagebox.showerror("Error","Something went wrong")
 
-    if (URL.find("mediamarkt") != -1):
-        # WORKING
-        # We get the product title and the price
-        try:
-            productTitle = soup.find("h1").get_text()
-            productPrice = soup.find('div', { "class" : "price"}).get_text()
-
-            printProductData(productTitle.strip(), productPrice.strip())
-
-            history.append(URL)
-
-        except AttributeError:
-            messagebox.showinfo("Info", "Product sold out or not trackable :(")
-        
-    elif (URL.find("aliexpress") != -1):
-        # NOT WORKING
-        # We get the product title and the price
-        #productPrice = soup.find('h1', { "class" : "product"}).get_text()
-        productTitle = soup.find('span', { "class" : "price"}).get_text()
-        
-        print("Nombre producto: "+productTitle.strip())
-        #print("Precio: "+productPrice.strip())
-    
-    elif (URL.find("worten") != -1):
-        # WORKING
-        # We get the product title and the price
-        try:
-            productTitle = soup.find("h1").get_text()
-            productPrice = soup.find('span', { "class" : "w-product__price__current"}).get_text()
-
-            printProductData(productTitle.strip(), productPrice.strip())
-
-            history.append(URL)
-        
-        except AttributeError:
-            messagebox.showinfo("Info", "Product sold out or not trackable :(")
-
-    elif (URL.find("pccomponentes") != -1):
-        # WORKING
-        # We get the product title and the price
-        try:
-            productTitle = soup.find("h1").get_text()
-            productPrice = soup.find('div', {"id" : "precio-main"}).get_text()
-
-            printProductData(productTitle.strip(), productPrice.strip())
-
-            history.append(URL)
-        
-        except AttributeError:
-            messagebox.showinfo("Info", "Product sold out or not trackable :(")
-
 root.mainloop()
+
+# Create a file to store favourite products
+with open("favouriteProducts.txt", "w") as f:
+    for product in favourite:
+        f.write(product + ",")
+    f.close()
